49c81703f2d885480e6dff95b9a63b5f755d6a4a
jjmartinmelero <juanjesusmelero@hotmail.com>
2025-12-02 13:32:49 +0100

    added first mcp

--- mcp-start/main.ts
+++ mcp-start/main.ts
@@ -1,4 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from 'zod'
 
 // 1. Create new server instance
 // this is the interface with MCP protocol. Managed communication with clients and servers
@@ -6,3 +8,27 @@ const server = new McpServer({
     name: "Demo",
     version: "1.0.0",
 });
+
+// define server tools 
+server.tool(
+    'fetch-weather', //title tool
+    'Tool to fetch the weather of a city', //description tool
+    {
+        city: z.string().describe('City name'), //params tool
+    },
+    async ({ city }) => {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: `The weather of ${city} is sunny`,
+                },
+            ],
+        }
+    }
+)
+
+
+// listen to the client connections
+const transport = new StdioServerTransport();
+await server.connect(transport);
